40a906a3fb2e4214ebf2a3604cfd5baa52d4a4f7
kraw084 <110000147+kraw084@users.noreply.github.com>
2024-07-05 16:39:03 +1200

    added absolute imports

--- models/tf.py
+++ models/tf.py
@@ -44,8 +44,8 @@ from models.common import (
 )
 from models.experimental import MixConv2d, attempt_load
 from models.yolo import Detect, Segment
-from utils.activations import SiLU
-from utils.general import LOGGER, make_divisible, print_args
+from yolov5.utils.activations import SiLU
+from yolov5.utils.general import LOGGER, make_divisible, print_args
 
 
 class TFBN(keras.layers.Layer):
